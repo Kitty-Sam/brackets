a4473413855629b4a13fa83924f8fa784857a909
kitty-sam <www.moonlight@mail.ru>
2021-03-11 17:07:38 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const firstCondition = bracketsConfig.map((item) => item.join(""));
+    for (let i = 0; i < firstCondition.length; i++) {
+      if (str.includes(firstCondition[i])) {
+          str = str.replace(firstCondition[i], "");
+          i = -1;
+      }
+  }
+  return str === "";
+};
